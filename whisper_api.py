539276680307126e6948c73f968d7d394082e8b9
1-Loneliness-1 <costya.kisurin@yandex.ru>
2025-04-19 18:44:51 +0300

    feat(whisper): load model for speech recognition

--- whisper_api.py
+++ whisper_api.py
@@ -1,6 +1,8 @@
 import openai
+import whisper
 from secret_api_key import WHISPER_API_KEY
 
 openai.api_key = WHISPER_API_KEY
 
-
+model = whisper.load_model("medium")
+result = model.transcribe(audio="audio.wav", language="ru", model=model)
